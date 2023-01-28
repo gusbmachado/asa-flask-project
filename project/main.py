0a04763cc2f073faba7bdd901330fc74e1dfa572
gusbmachado <gustavobmachado105@gmail.com>
2023-01-28 03:44:08 -0300

    add route

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from . import db
 from flask_login import login_required, current_user
-from .models import Flight
+from .models import Flight, Booking
 
 main = Blueprint('main', __name__)
 
@@ -10,8 +10,18 @@ def index():
 	return render_template('index.html')
 
 
-@main.route('/profile')
+@main.route('/profile', methods=["GET", "POST"])
 @login_required
 def profile():
 	flights = Flight.query.all()
-	return render_template('profile.html', name=current_user.name, flights=flights)
+	trips = Flight.get_routes()
+	airports = Flight.get_airports()
+	flightsTime = Flight.get_flights()
+	
+	return render_template('profile.html', name=current_user.name, flights=flights, trips=trips, airports=airports, flightsTime=flightsTime)
+
+@main.route('/dashboard/<name>/<flight>')
+@login_required
+def dashboard(name, flight):
+	Booking.book_flight(name, flight)
+	return 'Hi, %s. Get a ticket for %s' % (name, flight)
