4eb582d384bd7348b90af5ae8a325b5743a87bbd
gusbmachado <gustavobmachado105@gmail.com>
2023-01-29 22:49:00 -0300

    styles and routes update

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request
 from . import db
 from flask_login import login_required, current_user
-from .models import Flight, Booking
+from .models import Flight, Booking, Ticket
 
 main = Blueprint('main', __name__)
 
@@ -10,18 +10,47 @@ def index():
 	return render_template('index.html')
 
 
-@main.route('/profile', methods=["GET", "POST"])
+@main.route('/flights', methods=["GET", "POST"])
 @login_required
-def profile():
+def flights():
+	Flight.create_tables()
 	flights = Flight.query.all()
 	trips = Flight.get_routes()
 	airports = Flight.get_airports()
 	flightsTime = Flight.get_flights()
 	
-	return render_template('profile.html', name=current_user.name, flights=flights, trips=trips, airports=airports, flightsTime=flightsTime)
+	return render_template('flights.html', name=current_user.name, flights=flights, trips=trips, airports=airports, flightsTime=flightsTime)
 
 @main.route('/dashboard/<name>/<flight>')
 @login_required
 def dashboard(name, flight):
 	Booking.book_flight(name, flight)
-	return 'Hi, %s. Get a ticket for %s' % (name, flight)
+	history = Booking.book_history()
+	h = history.pop()
+	return render_template('booking.html', name=name, flight=flight, history=h)
+
+@main.route('/dashboard/history')
+@login_required
+def booking_history():
+	history = Booking.book_history()
+	return render_template('check_booking.html', history=history)
+
+@main.route('/dashboard/cancel/<flightCode>/<bookingId>')
+@login_required
+def cancel(flightCode, bookingId):
+	Booking.book_cancel(flightCode, bookingId)
+	return render_template('cancel.html', flightCode=flightCode)
+
+@main.route('/ticket/<bookingId>')
+@login_required
+def ticket(bookingId):
+	Ticket.set_ticket(bookingId)
+	tickets = Ticket.get_ticket()
+	t = tickets.pop()
+	return render_template('tickets.html', tickets=t)
+
+@main.route('/ticket/history')
+@login_required
+def ticket_history():
+	tickets = Ticket.get_ticket()
+	return render_template('check_tickets.html', tickets=tickets)
